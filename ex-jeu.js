5d37d71cec0852e105e9860c5bc72c68d8b08bb8
Your Name <you@example.com>
2019-01-30 12:05:28 +0100

    commit

--- ex-jeu.js
+++ ex-jeu.js
@@ -5,6 +5,25 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+/**
+ * Affichage d'intro du jeu
+ */
 const showGame = () => {
     console.log('\n**** ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ?? ****');
     console.log('**** Bienvenue sur le chiffre mystère ****');
@@ -19,62 +38,76 @@ const showGame = () => {
 const getRandomInt = (max) => Math.floor(Math.random() * Math.floor(max) + 1);
 
 /**
+ * Liste des nombres déjà testés
+ */
+let aNombres = [];
+
+/**
  * Lance le jeu
- * @param {boolean} b
+ * @param {boolean} b permet d'afficher l'intro du jeu
  */
 const run = (b) => {
-    const aNombres = [];
+    // Lance l'affichage de début du jeu
     if (b) {
         showGame();
     }
     rl.question('Sur combien de chiffres voulez-vous lancer le jeu ? ', (max) => {
+        max = parseInt(max);
         if (isNaN(max)) {
             console.log("Veuillez inscrire un chiffre !");
             run(false);
         } else {
             let nb = getRandomInt(max);
-            console.log('debug : ' + nb);
-
-            /**
-             * Commence la séquence de question
-             * jusqu'a ce que le joueur trouve le chiffre mystère
-             */
-            function start() {
-                rl.question('Choisir un chiffre en 0 et ' + max + ' ? ', (rep) => {
-                    if (rep === 'exit') {
-                        console.log("\n @ + o_O\n");
-                        return rl.close();
-                    }
-                    rep = Number(rep);
-                    if (isNaN(rep)) {
-                        console.log("C'est un nombre");
-                        return sstart();
-                    }
-                    console.log("Rappel des chiffres : " + aNombres.join(', '));
-                    aNombres.push(rep);
-                    // Moins
-                    if (rep > nb) {
-                        console.log("C'est moins");
-                        return start();
-                    }
-                    // Plus
-                    if (rep < nb) {
-                        console.log("C'est plus");
-                        return start();
-                    }
-                    // Gagné
-                    console.log("C'est gagné : " + nb);
-                    rl.question('Recommencer ? ', (r) => {
-                        if (r.toUpperCase() === 'Y' || r.toUpperCase() === 'YES') {
-                            return run(false);
-                        }
-                        rl.close();
-                    });
-                });
-            }
-            start();
+            // console.log('debug : ' + nb);
+            console.log("\n");
+            start(max, nb);
         }
     });
 };
+
+/**
+ * Commence la séquence de question
+ * jusqu'a ce que le joueur trouve le chiffre mystère
+ * @param {number} max
+ * @param {number} nb
+ */
+const start = (max, nb) => {
+    rl.question('Choisir un chiffre en 0 et ' + max + ' ? ', (rep) => {
+        if (rep === 'exit') {
+            exitMsg();
+            return rl.close();
+        }
+        rep = parseInt(rep);
+        if (isNaN(rep)) {
+            console.log("C'est un nombre");
+            return start(max, nb);
+        }
+        console.log("Rappel des chiffres : " + aNombres.join(', '));
+        aNombres.push(rep);
+        // Moins
+        if (rep > nb) {
+            console.log("C'est moins");
+            return start(max, nb);
+        }
+        // Plus
+        if (rep < nb) {
+            console.log("C'est plus");
+            return start(max, nb);
+        }
+        // Gagné
+        console.log("C'est gagné : " + nb + "\n");
+        rl.question('Recommencer Y/n ? ', (r) => {
+            r = r ? r.toUpperCase() : 'Y';
+            if (r === 'Y' || r === 'YES') {
+                aNombres = [];
+                return run(false);
+            }
+            exitMsg();
+            rl.close();
+        });
+    });
+}
+
+const exitMsg = () => console.log("\n @+ o_O\n");
 
 run(true);
---
+++ ex-jeu2.js
@@ -0,0 +1,97 @@
+(function () {
+    'use strict'
+
+    function getRandom() {
+        return Math.random();
+    }
+
+    const readline = require('readline');
+
+    const random = {
+        getArbitrary: (min, max) => {
+            return Math.random() * (max - min) + min;
+        },
+        getInt: (min, max) => {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min)) + min;
+        },
+        getIntInclusive: (min, max) => {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+    };
+
+    /**
+     *
+     * @param {number} options.min
+     * @param {number} options.max
+     */
+    function Jeu(options = {}) {
+        // const {min: min = 0, max: max = 100} = options;
+        const {min = 0, max = 100} = options;
+        // this._min = options.min || 0;
+        // this._max = options.max === undefined ? 100 : options.max;
+        this._essais = [];
+        this._entierAlea = random.getIntInclusive(this._min, this._max);
+        this._rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+    }
+    Jeu.prototype.setMin = function (min) {
+        this._min = min;
+        return this;
+    }
+    Jeu.prototype.setMax = function (max) {
+        this._max = max;
+        return this;
+    }
+    Jeu.prototype.generateNumber = function (typeRandom) {
+        this._entierAlea = random[typeRandom](this._min, this._max);
+        return this;
+    }
+    Jeu.prototype.jouer = function () {
+
+        let entierAlea = this._entierAlea;
+        let essais = this._essais;
+        let rl = this._rl;
+
+        if (essais.length) {
+            console.log(`Vous avez déjà joué : ${essais.join(' - ')}`);
+        }
+        rl.question('Quel est le nombre entier ? ', (answer) => {
+            const entierSaisi = parseInt(answer);
+            if (isNaN(entierSaisi)) {
+                console.log('Erreur : il faut saisir un nombre');
+                return this.jouer();
+            }
+
+            essais.push(entierSaisi);
+
+            if (entierSaisi < entierAlea) {
+                console.log('Trop petit');
+                return this.jouer();
+            }
+
+            if (entierSaisi > entierAlea) {
+                console.log('Trop grand');
+                return this.jouer();
+            }
+
+            console.log('Gagné');
+            rl.close();
+        });
+
+    }
+
+    const game = new Jeu({
+        min: 0,
+        max: 100
+    })
+    game
+        .generateNumber('getInt')
+        .jouer();
+
+}());
